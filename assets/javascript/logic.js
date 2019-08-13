ad076d74363e5c8026498c21c44789a6fde11b52
LahRee <Laurie.Strecker@gmail.com>
2019-08-13 12:45:15 -0700

    updated html, console.log not working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,16 +1,3 @@
-//PSEUDOCODE
-
-
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-// 1. Initialize Firebase
 var firebaseConfig = {
   apiKey: "AIzaSyC5ieIalTdQcj2oIWg6gqTosEhjQGMzY3A",
   authDomain: "trianscheduler.firebaseapp.com",
@@ -23,95 +10,18 @@ var firebaseConfig = {
  
   firebase.initializeApp(config);
   
-  var database = firebase.database();
+  var trainData = firebase.database();
   
   // 2. Button for adding trains
-  $("#add-train-btn").on("click", function(event) {
-    event.preventDefault();
-  
+  $("#addTrainBtn").on("click", function(){
+   
     // Grabs user input
-    var trainName = $("#train-name-input").val().trim();
-    var trainDestination = $("#destination-input").val().trim();
-    var trainFrequency = $("#frequency-input").val().trim();
-    var firstTrainTime = $("#firstTime-input").val().trim(); 
-  
-    // Creates local "temporary" object for holding train data
-    var newTrain = {
-      name: trainName,
-      destination: trainDestination,
-      frequency: trainFrequency,
-      firstTime: firstTrainTime
-    };
-    console.log(newTrain)
-    // Uploads train data to the database
-    database.ref().push(newTrain);
-  
-    // Logs everything to console
-  
-  
-    // Clears all of the text-boxes
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#frequency-input").val("");
-    $("#next-input").val("");
+    var trainName = $("#trainNameInput").val().trim();
+    var trainDestination = $("#destinationInput").val().trim();
+    var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X"); 
+    var frequency = $("#frequencyInput").val().trim();
+    
+    console.log(firstTrain);
+    return false;
+    
   });
-  
-  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-  
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var trainDestination = childSnapshot.val().destination;
-    var tFrequency = childSnapshot.val().frequency;
-    var firstTime = childSnapshot.val().firstTime;
-  
-    // Employee Info
-    console.log({trainName});
-    console.log({trainDestination});
-    console.log({tFrequency});
-    console.log({firstTime});
-  
-
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
-    // Current Time
-    var currentTime = moment();
-    console.log(currentTime);
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-    // Calculate the total billed rate
-    var nextArrival = moment(nextTrain).format("hh:mm A")
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(trainDestination),
-      $("<td>").text(tFrequency),
-      $("<td>").text(nextArrival),
-      $("<td>").text(tMinutesTillTrain),
-    );
-  
-    // Append the new row to the table
-    $("#train-table > tbody").append(newRow);
-  });
-
-
-
